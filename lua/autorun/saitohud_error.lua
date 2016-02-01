e6b72527729bab431e713172139e7a5100d1303a
CaveeJohnson <caveejohnson@hotmail.com>
2016-02-01 21:18:27 +0000

    [Added] experimental error handler

---
+++ lua/autorun/saitohud_error.lua
@@ -0,0 +1,59 @@
+-- SaitoHUD
+-- Copyright (c) 2016 Hexahedronic <http://www.hexahedronic.org>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- $Id: saitohud_error.lua GIT 2016-02-01 18:54:11Z hexahedronic $
+
+local function errorCallback(err)
+	ErrorNoHalt("[ERROR] SaitoHUD Encountered the following error at RunTime:\n")
+		print("\t" .. err)
+
+	ErrorNoHalt("\nPlease do the following:\n")
+		print("\t- Remain calm, don't go spamming the devs with 'HELP ERROR IT NO WORK'.")
+		print("\t- Visit the issue tracker. (https://github.com/hexahedronic/saitohud/issues)")
+		print("\t- Check if this error, or your issue has ALLREADY BEEN REPORTED, if it has...")
+		print("\t\t- Check your version of SaitoHUD is up-to-date.")
+		print("\t\t- If it is, try your best to resolve the issue yourself then wait for a proper fix.")
+		print("\t- If it has not been reported...")
+		print("\t\t- Create a new issue with the information in the error dump below.")
+
+	ErrorNoHalt("\nError Dump:\n")
+	print("--COMMENCE ERROR INFORMATION DUMP--")
+
+	print("\n--[[GM-VERSION: ".. VERSION .. "]]")
+	print("--[[OS: " .. jit.os .. ", ARCH: " .. jit.arch .. "]]\n")
+
+	print(err)
+
+	if SaitoHUD and SaitoHUD.AntiUnfairTriggered then
+		print("\n--[[AUT: " .. tostring(SaitoHUD.AntiUnfairTriggered()) .. "]]")
+
+	print("\n--END ERROR INFORMATION DUMP--")
+
+end
+
+function saitohud_errorhandle(func)
+	local details = {xpcall(func, errorCallback)}
+	local passed 	= details[1]
+
+	table.remove(details, 1)
+
+	if passed then
+		-- Maybe cache the datatypes/values to return fake
+		-- values if an error occurs?
+	end
+
+	return unpack(details)
+end
